6334c0e1fdac3d3a054543fc137e986283059afe
Przemek <pk.korzen1@gmail.com>
2022-01-15 14:46:40 +0100

    remove scanner and amend main method to use command line arguments

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -7,27 +7,31 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
-import java.util.Scanner;
 
 public class MovieApplication {
     //TODO: language choice (could be connected wtih properties)
-    //TODO: list of titles as arguments
     //TODO: exceptions
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Provide movie name in English");
-        String searchExpression = scanner.next();
-        System.out.println("Provide path where files will be saved");
-        String path = scanner.next();
+        String[] searchExpressions;
+        String path;
+        if (args.length > 0) {
+            path = args[0];
+            searchExpressions = new String[args.length-1];
+            System.arraycopy(args, 1, searchExpressions, 0, args.length-1);
+
+        } else throw new IllegalArgumentException("No arguments provided");
+
         MovieService movieService = new IMDBService();
         try {
-            List<SearchResultTitleDTO> titlesFound = movieService.findByExpression(searchExpression);
-            for (SearchResultTitleDTO title : titlesFound) {
-                MovieDTO movie = movieService.findById(title.getId());
-                BufferedWriter writer = new BufferedWriter(new FileWriter(path +
-                        movie.getTitle().replaceAll("[:|\\\\/]", "") + "-" + movie.getYear() + ".txt"));
-                writer.write(movie.toString());
-                writer.close();
+            for (String searchExpression : searchExpressions) {
+                List<SearchResultTitleDTO> titlesFound = movieService.findByExpression(searchExpression);
+                for (SearchResultTitleDTO title : titlesFound) {
+                    MovieDTO movie = movieService.findById(title.getId());
+                    BufferedWriter writer = new BufferedWriter(new FileWriter(path +
+                            movie.getTitle().replaceAll("[:|\\\\/]", "") + "-" + movie.getYear() + ".txt"));
+                    writer.write(movie.toString());
+                    writer.close();
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
